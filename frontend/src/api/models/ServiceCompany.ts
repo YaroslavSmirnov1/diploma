f189e4608e987ccb23de2672c5b8ab5951dedd2f
YaroslavSmirnov1 <89937201+YaroslavSmirnov1@users.noreply.github.com>
2023-03-08 20:49:55 +0300

    Initial commit

---
+++ frontend/src/api/models/ServiceCompany.ts
@@ -0,0 +1,10 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type ServiceCompany = {
+    readonly name?: string;
+    title: string;
+    description?: string | null;
+    readonly id?: number;
+};
